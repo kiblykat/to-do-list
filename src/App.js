430255a2da8b0419d05e1e1322ab17f881d1a7c4
izzat <izzatfadzlon@gmail.com>
2023-09-15 16:05:27 +0800

    checking and deleting functions working

--- src/App.js
+++ src/App.js
@@ -3,17 +3,39 @@ import './App.css'
 
 export default function App(){
   const [newItem,setNewItem] = useState('')     //this creates the initial state for newItem, an empty string
-  const [todoList,setTodoList] = useState([])   //this creates the initial state for todoList, an empty list
+  const [todos,setTodos] = useState([])   //this creates the initial state for todos, an empty list
 
   function handleSubmit(e){
     e.preventDefault()
 
-    setTodoList(currentTodoList => {
-      return[...currentTodoList, {id:crypto.randomUUID(), title:newItem, completed:false}]
+    setTodos(currentTodos => {
+      return[
+        ...currentTodos, 
+        {id:crypto.randomUUID(), title:newItem, completed:false},
+      ]
     })
 
     setNewItem('')
   }
+
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(elem => {
+        if(elem.id === id){
+          return { ...elem,completed };
+        }       
+        
+        return elem
+      })
+    })
+  }
+
+  function handleDelete(id){ 
+    setTodos(todos => {
+      return todos.filter(todo => todo.id !== id)   //filtering through todos and check if equal to deleted elem id.
+      })
+    }        
+  
   return (
     <>      
       <form onSubmit={handleSubmit} className='new-item-form'>
@@ -29,15 +51,19 @@ export default function App(){
       </form>
       <h1 className='header'>Todo List</h1>
       <ul className='list'>
-        {todoList.map(elem => {
-        return <li key = {elem.id}>      {/*ensure to include key for any map for optimization*/}
+        {todos.map(elem => {
+        return( 
+          <li key = {elem.id}>      {/*ensure to include key for any kinda map function*/}
             <label>
-              <input type='checkbox' id='item' />
+              <input 
+                type='checkbox' 
+                checked={todos.completed} 
+                onChange={e => toggleTodo(elem.id, e.target.checked)}/>
                 {elem.title}
             </label>
-            <button className='btn btn-danger'>Delete</button>
+            <button className='btn btn-danger' onClick={() => handleDelete(elem.id)}>Delete</button>
           </li>  
-        }
+        )}
           )}
       </ul>
     </>
